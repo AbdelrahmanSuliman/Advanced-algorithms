a6704a8dfac01e661348a1f53f9d7a21080825a6
AbdelrahmanSuliman <138822170+AbdelrahmanSuliman@users.noreply.github.com>
2024-12-12 19:12:19 +0200

    Update Task4.cpp

--- Task4.cpp
+++ Task4.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 
+int move_count = 0;
 void restricted_tower_of_hanoi(int n, char src, char aux, char dst) {
     if (n == 0) return;
 
     restricted_tower_of_hanoi(n - 1, src, aux, dst);
 
     std::cout << "Disk " << n << " moved from " << src << " to " << aux << std::endl;
+    move_count++;
 
     restricted_tower_of_hanoi(n - 1, dst, aux, src);
 
     std::cout << "Disk " << n << " moved from " << aux << " to " << dst << std::endl;
+    move_count++;
 
     restricted_tower_of_hanoi(n - 1, src, aux, dst);
 }
 
 int main() {
     int n = 3;
-    restricted_tower_of_hanoi(n, 'A', 'B', 'C');
+    restricted_tower_of_hanoi(n, 'A', 'B', 'C');\
+    std::cout << std::endl << move_count;
     return 0;
+    
 }
